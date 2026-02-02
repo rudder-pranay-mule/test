2e67e064042c1a71dbfc0ff1ff0048af96bf1875
Pranay R Mule <pranay.mule@rudderanalytics.com>
2026-02-02 12:47:05 +0000

    updated customers

--- models/dbtlearning/stg_customers.sql
+++ models/dbtlearning/stg_customers.sql
@@ -1,7 +1,6 @@
 select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
+    id as customer_id,
+    first_name,
+    last_name
 
-from {{ source('jaffle_shop', 'orders') }}
+from {{ source('jaffle_shop', 'customers') }}
